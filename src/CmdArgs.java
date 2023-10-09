4042a432b822e0b750eda8018cdf0635dfe1ab38
SCHROEDERJM22 <jacob.schroeder@comcast.net>
2023-10-09 13:31:56 -0400

    ADDed hi2

--- src/CmdArgs.java
+++ src/CmdArgs.java
@@ -4,6 +4,6 @@ public class CmdArgs {
     public static void main(String [] args){
         System.out.println("What is your name");
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Hi "+scnr.next());
+        System.out.println("hELLO "+scnr.next());
     }
 }
